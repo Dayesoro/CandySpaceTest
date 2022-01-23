69199c1c4ee60a8aed63e4477ee91348e765da8f
Daniel Ayesoro <dayesoro@gmail.com>
2022-01-23 19:26:43 +0000

    added userViewModel

---
+++ app/src/main/java/com/example/candyspace/ui/UserViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.candyspace.ui
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class UserViewModel:ViewModel() {
+
+}
